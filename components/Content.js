9fe4c2564f4687ab6d7706a9a087f753601e5ab0
Jamal Pryor <jamal.pryor@hotmail.com>
2018-08-29 20:39:19 -0500

    This is the updated version of site

--- components/Content.js
+++ components/Content.js
@@ -4,9 +4,21 @@ export default `
 
 <div id="greeting"></div>
         <div>
+        <h3>Below is my job description from LinkedIn</h3>
        <p>Operations Manager with a demonstrated history of working in the ATM industry. Skilled in ATM Software Configuration, Process Improvement, Management, Application Development, and Incident Management. Strong professional with an Associate of Arts and Sciences (AAS) focused in Information Technology from ITT Technical Institute and C# certification.</p>
         <h3>Test Text from a Webinar I previously hosted:</h3>
         <p>Jamal’s SharePoint expertise helps him identify the best way businesses can benefit from outsourcing their SharePoint management by constructing a solution to their exact specifications and requirements. Before he was architecting SharePoint solutions, Jamal was engineering them as a member of Fpweb.net’s Support Engineering Team where he provided real-time support for any SharePoint issue customers encountered. In Jamal’s spare time, he enjoys songwriting/ music production and he’s an active ASCAP member (The American Society of Composers, Authors, and Publishers).</p>
+
+        <h3>Below is old job description data from LinkedIn</h3>
+        <p>As a SharePoint Architect, I use my SharePoint expertise to identify how businesses can benefit from outsourcing their on-premise SharePoint environments and then develop a solution that is deployed to their exact business requirements and specifications . Prior to helping organizations save money via Cloud adoption, I was saving them time and internal resources as a SharePoint Support Engineer, working around the clock providing real-time phone and ticket support for any SharePoint/Connectivity issues customers encountered, so no automated systems ever got in the way. In both fields, my focus is customer satisfaction and building excellent long-term relationships. </p>
+
+        <h3><a href="https://www.fpweb.net/sharepoint-blog/upcoming-fpwebinar-the-battle-of-the-clouds/">Link To a Webinar I Hosted with Fpweb</a></h3>
+        <p>This month, we take a whole new approach to the classic webinar with our series <strong>“Steven &amp; Jamal Discuss the Cloud.”</strong> Don’t expect PowerPoint slides driven by monotone narration – we’re mixing it up with a 20 minute live presentation featuring our friendly Cloud experts, Steven Rupp and Jamal Pryor.</p>
+
+        <p><em>(This webinar is currently available to view on-demand:&nbsp;<a title="Battle of the Clouds Webinar" href="https://www.fpweb.net/sharepoint-webinars/battle-of-the-clouds-public-vs-private-cloud-hosting/" target="_blank">BATTLE OF THE CLOUDS: PUBLIC VS. PRIVATE CLOUD HOSTING</a>)</em></p>
+
+        <p>Join us on <strong>June 27th, 2013, at 11:00am (ET)</strong> for the first ‘Fpwebinar’ as we present <strong>“The Battle of the Clouds: Public vs. Private”</strong> – an informal, educational chat about the differences between the shared cloud model and the dedicated cloud model. With a cloud computing market that Gartner predicts will reach <strong>$150 billion</strong> this year, businesses are making the shift to the cloud and Jamal and Steven show you which is the right fit for you.</p>
+
    
 
 
